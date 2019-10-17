5eb03fd523209184058d340804d79cdb2009bdbd
W. de Oliveira Filho <w.de.oliveira.filho@avanade.com>
2019-10-17 11:49:38 -0300

    Add project files.

---
+++ ConsoleAppTeste/Program.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace ConsoleAppTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string resposta;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://wmof.com.br/api/produto/read1.php");
+            request.Method = "GET";
+            request.Accept = "application/json";
+
+            WebResponse response = request.GetResponse();
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                resposta = reader.ReadToEnd();
+            }
+        
+            List<Produto> produtos = JsonConvert.DeserializeObject<List<Produto>>(resposta);
+
+            Console.WriteLine(resposta);
+        }
+    }
+    class Produto
+    {
+        public int id;
+
+        public string nome;
+
+        public string descricao;
+
+        public string categoria;
+
+        public string subcategoria;
+
+        public double valor;
+
+        public string img;
+
+        public string estabelecimento_id;
+
+        public string estoque;
+    }
+}
